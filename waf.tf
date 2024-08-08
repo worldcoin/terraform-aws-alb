545f42dc181251d2ec87012741118ecd8c2ad493
Radoslaw Jozwik <radoslaw.jozwik@letsgodevops.pl>
2024-08-08 08:54:42 +0200

    add sqli

--- waf.tf
+++ waf.tf
@@ -82,6 +82,24 @@ resource "aws_wafv2_web_acl" "this" {
       sampled_requests_enabled   = true
     }
   }
+  rule {
+    name     = "AWSManagedRulesSQLiRuleSet"
+    priority = 4
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = format("%s-alb-waf-AWSManagedRulesSQLiRuleSet", local.name)
+      sampled_requests_enabled   = true
+    }
+  }
 }
 
 resource "aws_wafv2_web_acl_association" "this" {
