248d6e565cd99b8662f93f75e4275b56a234c66b
Tomasz Charewicz <tomasz.charewicz@toolsforhumanity.com>
2025-11-18 12:39:28 +0100

    remove provider

--- versions.tf
+++ versions.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 4.14.0"
-      configuration_aliases = [aws, aws.internal-tools]
+      source  = "hashicorp/aws"
+      version = ">= 4.14.0"
     }
 
     cloudflare = {
