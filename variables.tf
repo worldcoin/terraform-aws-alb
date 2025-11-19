fe3f0af9ba580eae24253f8bf87f885c26dcee45
Tomasz Charewicz <tomasz.charewicz@toolsforhumanity.com>
2025-11-19 17:58:10 +0100

    bugfixes usage of region

--- variables.tf
+++ variables.tf
@@ -115,7 +115,7 @@ variable "region" {
 variable "ca_s3_bucket" {
   description = "S3 bucket where the CA certificates for mTLS are stored"
   type        = string
-  default     = "wld-mtls-ca-${var.region}"
+  default     = "wld-mtls-ca"
 }
 
 variable "ca_s3_key" {
